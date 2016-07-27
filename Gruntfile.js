fa7a8fd433e832489a504f14d84c89be5bfefc62
Maurice Wright <me@mauricewright.info>
2016-07-27 11:33:17 -0400

    Updated modules in Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,25 @@ module.exports = function(grunt) {
         src: 'template/js/<%= pkg.name %>.js',
         dest: 'template/js/<%= pkg.name %>.min.js'
       }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+        },
+        files: {
+          'template/css/build.css': 'template/css/sass/main.scss'
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify','sass']);
 
 };
