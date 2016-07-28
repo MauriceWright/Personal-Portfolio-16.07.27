7b28559b9de0d974ec8d276cc6f008a829918aaf
Maurice Wright <me@mauricewright.info>
2016-07-28 13:59:50 -0400

    Separated PHP files

---
+++ template/header.php
@@ -0,0 +1,54 @@
+<!doctype html>
+
+<html class="no-js" lang="en-US">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Maurice Wright &bull; Innovation through code.</title>
+        <meta name="description" content="Portfolio for innovator, Maurice Wright, of Tallahassee, FL. who uses code as a brush on the canvas of the web.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#23c453">
+        <meta name="theme-color" content="#ffffff">
+
+        <link rel="stylesheet" href="css/build.css">
+        <!-- <script src="js/vendor/modernizr-2.8.3.min.js"></script> -->
+    </head>
+
+    <body>
+    <!--[if lt IE 8]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <header class="header-wrap">
+        <div class="header-bar">
+            <!-- Logo link -->
+            <div class="header-logo">
+                <h1><a href="" title="MauriceWright.info">MauriceWright.info</a></h1>
+                <h2><a href="" title="MauriceWright.info">Maurice Wright<span>-Front-end Designer &amp; Developer</span></a></h2>
+            </div>
+
+            <!-- Menu button -->
+            <button class="menu-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 32"><path d="M0 6h28v6h-28v-6zM0 14h28v6h-28v-6zM0 22h28v6h-28v-6z"></path><path d="M31 14l6 6 6-6z"></path></svg>
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 32"><path d="M0 6h28v6h-28v-6zM0 14h28v6h-28v-6zM0 22h28v6h-28v-6z"></path><path d="M31 20l6-6 6 6z"></path></svg>
+            </button>
+        </div>
+
+        <nav class="header-nav">
+            <!-- Site navigation -->
+            <ul class="nav-bar">
+                <li><a class="nav-icon" href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 32"><path d="M7 4h-6c-0.55 0-1 0.45-1 1v22c0 0.55 0.45 1 1 1h6c0.55 0 1-0.45 1-1v-22c0-0.55-0.45-1-1-1zM6 10h-4v-2h4v2z"></path><path d="M17 4h-6c-0.55 0-1 0.45-1 1v22c0 0.55 0.45 1 1 1h6c0.55 0 1-0.45 1-1v-22c0-0.55-0.45-1-1-1zM16 10h-4v-2h4v2z"></path><path d="M23.909 5.546l-5.358 2.7c-0.491 0.247-0.691 0.852-0.443 1.343l8.999 17.861c0.247 0.491 0.852 0.691 1.343 0.443l5.358-2.7c0.491-0.247 0.691-0.852 0.443-1.343l-8.999-17.861c-0.247-0.491-0.852-0.691-1.343-0.443z"></path><path d="M29 27c0 0.552-0.448 1-1 1s-1-0.448-1-1c0-0.552 0.448-1 1-1s1 0.448 1 1z"></path></svg><span>Projects</span></a></li>
+
+                <li><a class="nav-icon" href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 32"><path d="M15 0v0c8.284 0 15 5.435 15 12.139s-6.716 12.139-15 12.139c-0.796 0-1.576-0.051-2.339-0.147-3.222 3.209-6.943 3.785-10.661 3.869v-0.785c2.008-0.98 3.625-2.765 3.625-4.804 0-0.285-0.022-0.564-0.063-0.837-3.392-2.225-5.562-5.625-5.562-9.434 0-6.704 6.716-12.139 15-12.139zM31.125 27.209c0 1.748 1.135 3.278 2.875 4.118v0.673c-3.223-0.072-6.181-0.566-8.973-3.316-0.661 0.083-1.337 0.126-2.027 0.126-2.983 0-5.732-0.805-7.925-2.157 4.521-0.016 8.789-1.464 12.026-4.084 1.631-1.32 2.919-2.87 3.825-4.605 0.961-1.84 1.449-3.799 1.449-5.825 0-0.326-0.014-0.651-0.039-0.974 2.268 1.873 3.664 4.426 3.664 7.24 0 3.265-1.88 6.179-4.82 8.086-0.036 0.234-0.055 0.474-0.055 0.718z"></path></svg><span>Thoughts</span></a></li>
+
+                <li><a class="nav-icon" href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M29 4h-26c-1.65 0-3 1.35-3 3v20c0 1.65 1.35 3 3 3h26c1.65 0 3-1.35 3-3v-20c0-1.65-1.35-3-3-3zM12.461 17.199l-8.461 6.59v-15.676l8.461 9.086zM5.512 8h20.976l-10.488 7.875-10.488-7.875zM12.79 17.553l3.21 3.447 3.21-3.447 6.58 8.447h-19.579l6.58-8.447zM19.539 17.199l8.461-9.086v15.676l-8.461-6.59z"></path></svg><span>Contact</span></a></li>
+            </ul>
+        </nav>
+    </header>
