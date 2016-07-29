56737123363309f8c81541c95b3ccedc6cb0e477
Maurice Wright <me@mauricewright.info>
2016-07-29 13:27:40 -0400

    Starting work on Last FM API area

--- template/contact.php
+++ template/contact.php
@@ -12,57 +12,57 @@
                 <h3>Connect with Me</h3>
                 <ul>
                     <li><a href="mailto:me@mauricewright.info" title="Send me an e-mail" class="email">
-                        <?php include('img/envelop.svg'); ?>
+                        <? include('img/envelop.svg'); ?>
                         <span>Send me an e-mail</span>
                     </a></li>
 
                     <li><a href="" title="Subscribe to my RSS Feed" class="rss">
-                        <?php include('img/rss.svg'); ?>
+                        <? include('img/rss.svg'); ?>
                         <span>Subscribe to my RSS Feed</span>
                     </a></li>
 
                     <li><a href="https://www.facebook.com/ki4pzs" title="Like my Facebook" class="facebook">
-                        <?php include('img/facebook.svg'); ?>
+                        <? include('img/facebook.svg'); ?>
                         <span>Like my Facebook</span>
                     </a></li>
 
                     <li><a href="https://www.twitter.com/ki4pzs" title="Follow me on Twitter" class="twitter">
-                        <?php include('img/twitter.svg'); ?>
+                        <? include('img/twitter.svg'); ?>
                         <span>Follow me on Twitter</span>
                     </a></li>
 
                     <li><a href="https://plus.google.com/+MauriceWrightki4pzs/about/p/pub" title="Join my circle on Google" class="google">
-                        <?php include('img/google.svg'); ?>
+                        <? include('img/google.svg'); ?>
                         <span>Join my circle on Google</span>
                     </a></li>
 
                     <li><a href="https://www.linkedin.com/in/MauriceWright-ki4pzs" title="Connect with me on LinkedIn" class="linkedin">
-                        <?php include('img/linkedin.svg'); ?>
+                        <? include('img/linkedin.svg'); ?>
                         <span>Connect with me on LinkedIn</span>
                     </a></li>
 
                     <li><a href="https://www.github.com/MauriceWright" title="Fork my GitHub" class="github">
-                        <?php include('img/github.svg'); ?>
+                        <? include('img/github.svg'); ?>
                         <span>Fork my GitHub</span>
                     </a></li>
 
                     <li><a href="https://www.instagram.com/ki4pzs/" title="Follow my Instagram" class="instagram">
-                        <?php include('img/instagram.svg'); ?>
+                        <? include('img/instagram.svg'); ?>
                         <span>Follow my Instagram</span>
                     </a></li>
 
                     <li><a href="https://www.flickr.com/photos/ki4pzs/" title="View my Flickr" class="flickr">
-                        <?php include('img/flickr.svg'); ?>
+                        <? include('img/flickr.svg'); ?>
                         <span>View my Flickr</span>
                     </a></li>
 
                     <li><a href="http://www.last.fm/user/ki4pzs" title="Hear my favs on Last FM" class="lastfm">
-                        <?php include('img/lastfm.svg'); ?>
+                        <? include('img/lastfm.svg'); ?>
                         <span>Hear my favs on Last FM</span>
                     </a></li>
 
                     <li><a href="https://soundcloud.com/maurice-wright-858022962" title="Hear my favs on SoundCloud" class="soundcloud">
-                        <?php include('img/soundcloud.svg'); ?>
+                        <? include('img/soundcloud.svg'); ?>
                         <span>Hear my favs on SoundCloud</span>
                     </a></li>
                 </ul>
@@ -71,29 +71,9 @@
             <!-- Last FM feed -->
             <div class="feed-lastfm">
                 <h3>Recent tracks on Last FM</h3>
+                <? $fm = getLastFm(); ?>
                 <ul class="fm-tracks">
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
-                    <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
-
+                    <!-- Last FM API feed -->
                     <li class="fm-track"><a href=""><img src="" alt="" class="fm-image"></a></li>
                 </ul>
             </div>
