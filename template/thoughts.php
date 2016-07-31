c41b071701df0dc8abcb5022834737316c8e2e3b
Maurice Wright <me@mauricewright.info>
2016-07-31 15:21:58 -0400

    Started Thoughts page

---
+++ template/thoughts.php
@@ -0,0 +1,12 @@
+<?
+    $page='thoughts';
+    include('header.php');
+    include('script/functions.php');
+?>
+
+
+        <!-- Content area -->
+        <div class="page-wrap thoughts-wrap">
+        </div>
+
+<?php $FClass='footer-thoughts'; include('footer.php'); ?>
