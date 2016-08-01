979f11da14cd0ff8074e969529b21213c9454c22
Maurice Wright <me@mauricewright.info>
2016-08-01 12:13:14 -0400

    Working on Single Post page

---
+++ template/thought.php
@@ -0,0 +1,53 @@
+<?
+    $page='thought';
+    include('header.php');
+?>
+
+
+        <!-- Content area -->
+        <div class="page-wrap thought-wrap">
+            <!-- Thought -->
+            <div class="my-thought">
+                <article class="thought-article">
+                    <h3>Police Kill Mamba</h3>
+                    <time datetime="2016-07-31 15:26 EST">July 31, 2016</time>
+
+
+                    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+
+                    <ol>
+                       <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                       <li>Aliquam tincidunt mauris eu risus.</li>
+                    </ol>
+
+                    <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+
+                    <h3>Header Level 3</h3>
+
+                    <ul>
+                       <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                       <li>Aliquam tincidunt mauris eu risus.</li>
+                    </ul>
+
+                    <div class="image">
+                        <img src="img/me_use_1024_H.jpg" alt="A photo of me.">
+                        <p>A picture of me</p>
+                        </div>
+<script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
+                    <h4>Hello Nurse</h4>
+
+                    <h5>Pinky wuz here</h5>
+
+                    <pre><code>
+#header h1 a {
+    display: block;
+    width: 300px;
+    height: 80px;
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ex numquam non tenetur alias eveniet possimus, qui fugiat beatae, facere sit voluptatibus. Pariatur, rerum, commodi? Dolore aliquid labore, sunt sit.
+}
+                    </code></pre>
+                </article>
+            </div>
+        </div>
+
+<?php $FClass='footer-thought'; include('footer.php'); ?>
--- template/thoughts.php
+++ template/thoughts.php
@@ -1,7 +1,6 @@
 <?
     $page='thoughts';
     include('header.php');
-    include('script/functions.php');
 ?>
 
 
@@ -9,7 +8,7 @@
         <div class="page-wrap thoughts-wrap">
             <!-- Thoughts -->
             <article class="thought">
-                <h3><a href="">Random Article</a></h3>
+                <h3><a href="thought.php">Random Article</a></h3>
                 <time datetime="2016-07-31 15:26 EST">July 31, 2016</time>
                 <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
             </article>
